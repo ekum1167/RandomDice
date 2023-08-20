85b8454e8528decc65cb2a5508ffb6bd77bcb285
Ekin Oskay <ekin.oskay@sap.com>
2023-08-20 13:02:59 +0200

    Initial Commit

---
+++ RandomDice/RandomDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDiceApp.swift
+//  RandomDice
+//
+//  Created by Oskay, Ekin on 20.08.23.
+//
+
+import SwiftUI
+
+@main
+struct RandomDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
